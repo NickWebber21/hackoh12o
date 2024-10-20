6c7ed0b20a27b4562bdd6faf401b8e74568dec06
Nick Webber <nicholaswebbercse@outlook.com>
2024-10-19 22:27:45 -0400

    hours to mins stuff

--- ridepare/src/Footer.js
+++ ridepare/src/Footer.js
@@ -4,11 +4,7 @@ function Footer() {
   return (
     <footer className="bg-primary text-white py-4">
       <div className="container mx-auto text-center">
-        <p>&copy; 2023 RidePare. All rights reserved.</p>
-        <p className="mt-2">
-          <a href="#" className="hover:text-accent">Privacy Policy</a> |
-          <a href="#" className="hover:text-accent ml-2">Terms of Service</a>
-        </p>
+        <p>&copy; 2024 RidePare. All rights reserved.</p>
       </div>
     </footer>
   );
